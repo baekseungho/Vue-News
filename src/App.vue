9471414bb6581d497d128cc1f0e0ae8084f0c0d7
shbaek <shbaek@dainlab.com>
2023-06-14 19:39:31 +0900

    route

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
-  <div id="app">hello</div>
+    <div id="app">
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    fethData() {
-      console.log("hello");
-    },
-  },
-};
+export default {};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
