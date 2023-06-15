d86765def4db9141706db67047fc3415bfc24374
baekseungho <vudrkd1580@naver.com>
2023-06-15 21:12:34 +0900

    api test

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,12 @@ import VueRouter from "vue-router";
 import NewsView from "../views/NewsView.vue";
 import AskView from "../views/AskView.vue";
 import JobsView from "../views/JobsView.vue";
+import UserView from "../views/UserView.vue";
+import ItemView from "../views/ItemView.vue";
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
+  mode: "history", //주소창에서 해시값제거
   routes: [
     {
       path: "/",
@@ -24,6 +27,14 @@ export const router = new VueRouter({
     {
       path: "/jobs",
       component: JobsView,
+    },
+    {
+      path: "/user",
+      component: UserView,
+    },
+    {
+      path: "/item",
+      component: ItemView,
     },
   ],
 });
